0a68d63dc31303dc76824b259ca6597b80159fa3
Luke Baltay <luke.baltay@yale.edu>
2022-07-18 13:36:24 -0400

    commit tree 6c1478e2c6cf04bf3c3ac1b276e016b43988519a

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 
+function superbowlWin(arr) {
+    const bowl = arr.find(function(season) {return season.result === "W"});
+    if (bowl){
+        return bowl.year;
+    } else {
+        return undefined;
+    }
+    
+}
